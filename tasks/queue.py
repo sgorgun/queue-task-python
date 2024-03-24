c9a193d4e6a6b8fcc7598a726a87a34fb16ed982
sgorgun <sergei.gorgun@gmail.com>
2024-03-25 01:09:50 +0200

    Refactor queue and stack implementations

--- tasks/queue.py
+++ tasks/queue.py
@@ -1,35 +1,44 @@
 """Templates for programming assignments: queue API."""
 from typing import Any, Optional
 
-from tasks.stack import Stack
+from tasks.stack import Stack, LinkedListNode
 
-
+# The following snippet contains the default interface, which can be used to implement the queue data structure. Of course, the interface can be expanded with additional methods if necessary.
 class Queue:
     """Default interface for Queue data structure."""
 
     def __init__(self):
-        pass
+        self.head = None
+        self.tail = None
+        self._size = 0
 
     def empty(self) -> bool:
         """Returns True if the queue is empty.
 
         NOTE: O(1) complexity is expected for this operation.
         """
-        pass
+        return self._size == 0
 
     def size(self) -> int:
         """Returns the number of elements within the queue.
 
         NOTE: O(1) complexity is expected for this operation.
         """
-        pass
+        return self._size
 
     def push(self, element: Any):
         """Adds a given element to the queue's tail.
 
         NOTE: O(1) complexity is expected for this operation.
         """
-        pass
+        new_node = LinkedListNode(element)
+        if self.empty():
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_element = new_node
+            self.tail = new_node
+        self._size += 1
 
     def pop(self) -> Any:
         """Returns the head element and removes it.
@@ -39,7 +48,12 @@ class Queue:
         Raises:
             ValueError: If the queue is empty.
         """
-        pass
+        if self.empty():
+            raise ValueError
+        value = self.head.value
+        self.head = self.head.next_element
+        self._size -= 1
+        return value
 
     def peak(self) -> Any:
         """Returns the head element.
@@ -49,7 +63,9 @@ class Queue:
         Raises:
             ValueError: If the queue is empty.
         """
-        pass
+        if self.empty():
+            raise ValueError
+        return self.head.value
 
 
 class QueueViaStacks:
@@ -71,21 +87,21 @@ class QueueViaStacks:
 
         NOTE: O(1) complexity is expected for this operation.
         """
-        pass
+        return self.first_stack.empty() and self.second_stack.empty()
 
     def size(self) -> int:
         """Returns the number of elements within the queue.
 
         NOTE: O(1) complexity is expected for this operation.
         """
-        pass
+        return self.first_stack.size() + self.second_stack.size()
 
     def push(self, element: Any):
         """Adds a given element to the queue's tail.
 
         NOTE: O(1) complexity is expected for this operation.
         """
-        pass
+        self.first_stack.push(element)
 
     def pop(self) -> Any:
         """Returns the head element and removes it.
@@ -95,7 +111,12 @@ class QueueViaStacks:
         Raises:
             ValueError: If the queue is empty.
         """
-        pass
+        if self.empty():
+            raise ValueError("Queue is empty")
+        if self.second_stack.empty():
+            while not self.first_stack.empty():
+                self.second_stack.push(self.first_stack.pop())
+        return self.second_stack.pop()
 
     def peak(self) -> Any:
         """Returns the head element.
@@ -105,4 +126,9 @@ class QueueViaStacks:
         Raises:
             ValueError: If the queue is empty.
         """
-        pass
+        if self.empty():
+            raise ValueError("Queue is empty")
+        if self.second_stack.empty():
+            while not self.first_stack.empty():
+                self.second_stack.push(self.first_stack.pop())
+        return self.second_stack.peak()
--- tasks/stack.py
+++ tasks/stack.py
@@ -1,33 +1,50 @@
-from typing import Any
+"""Templates for programming assignments: stack API."""
+from typing import Any, Optional
 
+class LinkedListNode:
+    """Dataclass that represents linked list elements."""
 
-# Please refer to the Stack problem set. You may reuse the implementation for the class below.
+    def __init__(
+        self,
+        value: int = 0,
+        next_element: Optional['LinkedListNode'] = None
+    ):
+        self.value = value
+        self.next_element = next_element
+
 class Stack:
     """Default interface for Stack data structure."""
 
     def __init__(self):
-        pass
+        self.head = None
+        self.counter = 0
+        self.min_stack = None
 
     def empty(self) -> bool:
         """Returns True if the stack is empty.
 
         NOTE: O(1) complexity is expected for this operation.
         """
-        pass
+        return self.head is None
 
     def size(self) -> int:
         """Returns the number of elements within the stack.
 
         NOTE: O(1) complexity is expected for this operation.
         """
-        pass
+        return self.counter
 
     def push(self, element: Any):
         """Adds a given element to the top of the stack.
 
         NOTE: O(1) complexity is expected for this operation.
         """
-        pass
+        new_node = LinkedListNode(element, self.head)
+        self.head = new_node
+        self.counter += 1
+        
+        if self.min_stack is None or element <= self.min_stack.value:
+            self.min_stack = LinkedListNode(element, self.min_stack)
 
     def pop(self) -> Any:
         """Returns the top element and removes it.
@@ -37,7 +54,16 @@ class Stack:
         Raises:
             ValueError: If the stack is empty.
         """
-        pass
+        if self.empty():
+            raise ValueError("Stack is empty")
+        result = self.head.value
+        self.head = self.head.next_element
+        self.counter -= 1
+        
+        if result == self.min_stack.value:
+            self.min_stack = self.min_stack.next_element
+        
+        return result
 
     def peak(self) -> Any:
         """Returns the top element.
@@ -47,4 +73,22 @@ class Stack:
         Raises:
             ValueError: If the stack is empty.
         """
-        pass
+        if self.empty():
+            raise ValueError("Stack is empty")
+        return self.head.value
+
+class StackWithMinimum(Stack):
+    """Extended Stack class that supports `get_minimum` operation.
+
+    Assume that elements in Stack are numerical (so that `get_minimum` operation is eligible).
+    """
+
+    def get_minimum(self) -> Optional[int]:
+        """Returns the minimum element in the stack.
+
+        NOTE: if the stack is empty - return None.
+        NOTE: O(1) complexity is expected for this operation.
+        """
+        if self.empty():
+            return None
+        return self.min_stack.value
